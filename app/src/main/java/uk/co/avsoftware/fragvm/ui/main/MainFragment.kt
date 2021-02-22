c85cfed168eb8105a0c3faf3e8c8f67af4ed0809
Andrew Bennett <andrew.bennett3@argos.co.uk>
2021-02-22 21:09:45 +0000

    basic logging

--- app/src/main/java/uk/co/avsoftware/fragvm/ui/main/MainFragment.kt
+++ app/src/main/java/uk/co/avsoftware/fragvm/ui/main/MainFragment.kt
@@ -2,6 +2,7 @@ package uk.co.avsoftware.fragvm.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,8 @@ class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
+
+        const val TAG = "MainFragment"
     }
 
     private lateinit var viewModel: MainViewModel
@@ -25,6 +28,10 @@ class MainFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+
+        Log.i(TAG, "OnActivityCreated()")
     }
+
+
 
 }
