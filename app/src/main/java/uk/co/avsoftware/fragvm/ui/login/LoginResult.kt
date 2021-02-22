c6c87315cce20f97b259a843fe06edef20f1817b
Andrew Bennett <andrew.bennett3@argos.co.uk>
2021-02-22 21:18:31 +0000

    template classes for login

---
+++ app/src/main/java/uk/co/avsoftware/fragvm/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package uk.co.avsoftware.fragvm.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
