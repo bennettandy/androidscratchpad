24b7d341b7c5a26e579c8e94861bff93d9a6776d
Andrew Bennett <andrew.bennett3@argos.co.uk>
2021-02-26 15:31:16 +0000

    added data bindings

--- app/src/main/java/uk/co/avsoftware/fragvm/ui/main/MainFragment.kt
+++ app/src/main/java/uk/co/avsoftware/fragvm/ui/main/MainFragment.kt
@@ -8,37 +8,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import uk.co.avsoftware.fragvm.R
+import uk.co.avsoftware.fragvm.databinding.MainFragmentBinding
 import uk.co.avsoftware.fragvm.ui.login.LoginActivity
 
 class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    private lateinit var binding: MainFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        // inflate view via DataBindings
+        binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        Log.i(TAG, "OnActivityCreated()")
         // ViewModel, obtain or create new from factory
         viewModel = ViewModelProvider(this, MainViewModelFactory()).get(MainViewModel::class.java)
 
-        // TODO: Use the ViewModel
-        Log.i(TAG, "OnActivityCreated()")
+        binding.viewmodel = viewModel
 
-        if (!viewModel.isLoggedIn()) {
-            Log.i(TAG, "Not Logged In")
-            startActivity(Intent(context, LoginActivity::class.java))
+        when (viewModel.isLoggedIn()) {
+
+            true -> Toast.makeText(context, "XXX Logged In", Toast.LENGTH_LONG).show()
+
+            false -> Intent(context, LoginActivity::class.java).also {
+                startActivity(it)
+            }
+
         }
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.i(TAG, "On View Created")
+
+    }
 
     companion object {
         fun newInstance() = MainFragment()
-
         const val TAG = "MainFragment"
     }
 }
